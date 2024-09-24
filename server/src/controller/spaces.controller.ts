f9aa4be41e983f94991fcd7af70ec0caa3ba4508
Shashank Gupta <shashankgupta9248@gmail.com>
2024-09-25 02:39:27 +0530

    :truck:: complete create space

--- server/src/controller/spaces.controller.ts
+++ server/src/controller/spaces.controller.ts
@@ -6,9 +6,19 @@ export const createSpace = async (req: express.Request, res: express.Response) =
     const body = req.body;
     const { data, success, error } = spaceSchema.safeParse(body);
     if (!success) return res.status(400).json(error.issues);
-    
+
     const space = await prisma.space.create({
         data: data
     });
     res.status(200).json(space);
+}
+
+const getSpaces = async (req: express.Request, res: express.Response) => {
+    const { userId } = req.body;
+    const spaces = await prisma.space.findMany({
+        where: {
+            userId: userId
+        }
+    });
+    res.status(200).json(spaces);
 }
