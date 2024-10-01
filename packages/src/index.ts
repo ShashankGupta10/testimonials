8a5e0e06b28ec52d8211f6db240c7769c0c938d1
Shashank Gupta <shashankgupta9248@gmail.com>
2024-10-02 00:06:43 +0530

    :truck:: try to make vercel happy

--- packages/src/index.ts
+++ packages/src/index.ts
@@ -1,4 +1,26 @@
-import { RegisterUserType, LoginUserType, signupSchema, loginSchema } from "./auth";
-import { spaceSchema, SpaceType } from "./spaces";
+import { z } from "zod";
 
-export { RegisterUserType, LoginUserType, signupSchema, loginSchema, spaceSchema, SpaceType };
+const signupSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+    name: z.string(),
+});
+
+const loginSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+});
+
+const spaceSchema = z.object({
+    spaceName: z.string(),
+    headerTitle: z.string(),
+    customMessage: z.string(),
+    questions: z.array(z.string()),
+})
+
+type RegisterUserType = z.infer<typeof signupSchema>;
+type LoginUserType = z.infer<typeof loginSchema>;
+type SpaceType = z.infer<typeof spaceSchema>;
+
+export { signupSchema, loginSchema, spaceSchema };
+export type { RegisterUserType, LoginUserType, SpaceType };
